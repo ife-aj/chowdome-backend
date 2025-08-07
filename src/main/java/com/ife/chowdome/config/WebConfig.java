367882e7148afd158d80aafb58f0417c93efa2ae
ife-aj <ifeoluwaadeaynju6@gmail.com>
2025-08-07 14:49:35 +0100

    added netlify origin

--- src/main/java/com/ife/chowdome/config/WebConfig.java
+++ src/main/java/com/ife/chowdome/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // Live Server
+                        .allowedOrigins("http://127.0.0.1:5500",
+                        "https://storied-madeleine-8ad05f.netlify.app") // Live Server
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
